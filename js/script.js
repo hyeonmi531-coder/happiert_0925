21b8c5f13e503aa0cb3234ae7f8a0c46bb0a73ce
hyeomi531@gmail.com <hyeonmi531@gmail.com>
2025-10-17 19:12:09 +0900

    스와이퍼 여백,스크롤바,파비콘 생성

--- js/script.js
+++ js/script.js
@@ -2,10 +2,17 @@ function productListSwiperInit() {
   const swiper = new Swiper(".product-list-wrap .swiper", {
     loop: true /* 슬라이드 반복 여부 */,
     speed: 1000 /* 슬라이드 넘어가는 속도 */,
-    // spaceBetween: 20,
+    spaceBetween: 20,
     slidesPerView: "auto",
     slidesPerGroup: 1,
     breakpoints: {
+      // when window width is >= 1024px     화면의 너비가 1024px 이상일 때
+      720: {
+        slidesPerView: 2 /* 한 화면당 보여질 슬라이드 개수 */,
+        spaceBetween: 10 /* 슬라이드간의 간격 */,
+        slidesPerGroup: 3 /* 한 화면당 슬라이드시 넘어갈 슬라이드 개수 */,
+      },
+
       // when window width is >= 1024px 화면의 너비가 1024px 이상일 때
       1024: {
         slidesPerView: 3 /* 한 화면당 보여질 슬라이드 개수 */,
